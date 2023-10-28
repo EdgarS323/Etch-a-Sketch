48a06e9e25d7d0e2f22c8faf2862bd74b5dd49ec
Edgar Sanchez <egsanchez530@gmail.com>
2023-10-27 17:32:18 -0700

    Fixed function for adding Squares to grid. Made squares dynamically change size depending on grid size

--- app.js
+++ app.js
@@ -3,14 +3,11 @@
 const container = document.querySelector('#container');
 const squares = document.querySelectorAll('.square');
 const button = document.querySelector('#button');
+let gridSize = 50;
 
-// Event Listener to add divs to board on page load
+// Add Default grid
 
-window.addEventListener('load', () => {
-  for (let i = 1; i < 50; i++) {
-    createSquare();
-  }
-});
+createGrid(gridSize);
 
 // Event Listener to change square color using Event Delegation on the Container Parent Element
 
@@ -24,20 +21,35 @@ container.addEventListener(
   true
 );
 
-button.addEventListener('click', () => {
-  reset();
-  //   prompt('How big would you like your grid to be? (Maximum = 100)');
-});
+// Reset board to user inputted grid size
+
+// button.addEventListener('click', () => {
+//   reset();
+//   let gridSize = prompt('How big would you like your grid to be? (Maximum = 100)');
+//   createGrid(gridSize);
+// });
 
 //Functions
+
 function reset() {
   while (container.firstChild) {
     container.removeChild(container.lastChild);
   }
 }
 
-function createSquare() {
-  let newSquare = document.createElement('div');
-  newSquare.classList.add('square');
-  container.appendChild(newSquare);
+function createGrid(amount) {
+  for (let i = 0; i < amount; i++) {
+    let newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.style.width = `${700 / amount}px`;
+    newSquare.style.height = `${700 / amount}px`;
+    container.appendChild(newSquare);
+    for (let n = 0; n < amount; n++) {
+      let innerSquare = document.createElement('div');
+      innerSquare.classList.add('square');
+      innerSquare.style.width = `${700 / amount}px`;
+      innerSquare.style.height = `${700 / amount}px`;
+      newSquare.appendChild(innerSquare);
+    }
+  }
 }
